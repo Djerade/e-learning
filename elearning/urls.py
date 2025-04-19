d3c0ae4f1086e1e59d2abf935fadb22622017375
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2025-04-19 03:59:11 +0000

    update

--- elearning/urls.py
+++ elearning/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('courses/', include('courses.urls')),
+    path('chatbot/', include('chatbot.urls', namespace='chatbot')),
     path('', course_list, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
